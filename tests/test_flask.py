d393abd20b52a9075d697b710559ba429fbd5ef0
djcrabhat <djcrabhat@sosimplerecords.com>
2017-04-02 22:44:42 -0700

    ugggh

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -4,6 +4,12 @@ import os
 import unittest
 import tempfile
 
+# mangle the module on to the path
+import sys
+testdir = os.path.dirname(__file__)
+srcdir = '../'
+sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
+
 from ses_s3_inbox import create_app
 
 class FlaskrTestCase(unittest.TestCase):
